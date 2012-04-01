950d95f4ec23e7ac15e9ae9a90fb41a4e9ae914e
Ketura <hyperlink.teltura@gmail.com>
2012-03-31 21:10:44 -0600

    Further modified package structure to allow ease of installation; added setup.py and source tarball

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="Cable",
+      version="0.1",
+      description="Library for the C-like ABstraction Layout (Cable) human-readable data format",
+      author="Christian McCarty and Mark Owen",
+      author_email="hyperlink.teltura@gmail.com",
+      url="https://github.com/cableformat/cable",
+      packages=['Cable']
+      )
